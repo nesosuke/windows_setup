02fe2ec442ea30534912801d1b7596e6f137bf5b
nesosuke <neso@neso.tech>
2022-08-29 20:42:52 +0900

    add

---
+++ scoop_install.ps1
@@ -0,0 +1,14 @@
+scoop bucket add extras
+scoop bucket add nerd-fonts
+scoop install `
+7zip          `
+ghostscript   `
+iperf3        `
+latex         `
+nodejs        `
+miktex        `
+oh-my-posh    `
+pandoc        `
+unzip         `
+vim           `
+python        `
